8e80f6d1cb0ab1abbd10234b0d62d303d00fd70a
itkangkang <itkangkang@163.com>
2021-02-07 00:14:28 +0800

    后台显示

--- blog/home/admin.py
+++ blog/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from home.models import ArticleCategory, Article
+from home.models import ArticleCategory, Article,Comment
 
 # Register your models here.
 admin.site.register(ArticleCategory)
 admin.site.register(Article)
+admin.site.register(Comment)
